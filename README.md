edb5f3653af7ee76d555f2898cc76ebded5e8cbc
7YC3RG3N3 <4C@.b>
2024-12-23 15:52:47 +0100

    Updated i3 + zsh config

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Personnal configuration
+This is my personnal configuration.
+I'm actually using Ubuntu with :
+- I3wm as window manager
+- Zsh + powerlevel10k as terminal and theme
+- neovim as code editor
 
-## I3WM
-Window manager
-
-## ZSH
-
-## NVIM
+## Install
+Just run :
+```git clone git@github.com:salamientark/Full-config.git && cd Full-config && sudo -u $(whoami) ./install.sh```
---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Update and instal necessary program
+sudo apt update -y && sudo apt upgrade -y
+yes | sudo apt install -y curl wget git zsh vim tree i3
+
+# i3 Configuration
+cp ./i3/config ~/.config/i3/config
+
+#    ZSH
+# Installing Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp ./zsh/.zshrc ~/.
+
+# Installing fonts (For powerlevel10k)
+echo "Installing fonts"
+mkdir ~/.local/share/fonts
+cp ./p10k/fonts/* ~/.local/share/fonts/.
+fc-cache -f -v
+
+# Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+cp ./p10k/.p10k.zsh ~/.
+
+# Add zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
+# Reload i3
+i3-msg reload
+i3-msg restart
